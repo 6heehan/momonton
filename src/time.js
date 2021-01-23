dc4e30ce68a70cc5054e0d0b2ddece18ac8d3307
6heehan <6heehan@gmail.com>
2021-01-24 00:19:28 +0900

    Update time.js

--- src/time.js
+++ src/time.js
@@ -5,7 +5,7 @@ function getTime() {
   const hour = date.getHours();
   const min = date.getMinutes();
   
-  time.innerText = `${hour<10 ? `0${hour}` : hour}:${min<10 ? `0${min} : min}`;
+  time.innerText = `${hour<10 ? `0${hour}` : hour}:${min<10 ? `0${min}` : min}`;
 }
 function init() {
   getTime();
