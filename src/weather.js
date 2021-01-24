1de31aa0cd6378c6912c56820d9d26c8c2a858fc
6heehan <6heehan@gmail.com>
2021-01-24 19:22:33 +0900

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -7,9 +7,7 @@ function getWeather(lat, lon) {
     `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
     .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
+      const json = response.json();
       const temp = json.main.temp;
       const place = json.name;
       weather.innterText = `${temp} @ ${place}`;
